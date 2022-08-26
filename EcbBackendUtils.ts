e06027893a66dd08e4d709d85dddb6e0571a3b68
Jaakko Heusala <jhh@heusalagroup.fi>
2022-08-26 17:21:13 +0300

    Improve code style

--- EcbBackendUtils.ts
+++ EcbBackendUtils.ts
@@ -1,7 +1,6 @@
 // Copyright (c) 2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 
-import { Currency } from "../core/types/Currency";
-import { CurrencyRates } from "../core/types/CurrencyRates";
+import { createCurrencyRates, CurrencyRates } from "../core/types/CurrencyRates";
 import { fetch } from "ecb-euro-exchange-rates";
 
 export class EcbBackendUtils {
@@ -11,10 +10,10 @@ export class EcbBackendUtils {
      */
     static async fetchEuroRates () : Promise<CurrencyRates> {
         const result = await fetch();
-        return {
-            [Currency.USD]: result.rates.USD,
-            [Currency.GBP]: result.rates.GBP
-        };
+        return createCurrencyRates(
+            result.rates.USD,
+            result.rates.GBP
+        );
     }
 
 }
